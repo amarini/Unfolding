9c57cd5e7942937108cdfbea04d368194808ca44
GuillelmoGomezCeballos <ceballos@cern.ch>
2017-06-15 21:16:50 +0200

    new updates

--- macros/unfolding.C
+++ macros/unfolding.C
@@ -10,25 +10,25 @@
 #include <TROOT.h>
 // nsel = 0 (mm) 1 (ee)
 //LO=0 (LO) 1(NLO)
-void unfolding(int nsel = 0,int LO=0){
-  if (LO==0){
-    TFile *_file0 = TFile::Open("/afs/cern.ch/user/c/christiw/public/panda/MitAnalysisRunII/panda/macros/80x/LO/histozllPtRec.root");
-    TFile *_file1 = TFile::Open("/afs/cern.ch/user/c/christiw/public/panda/MitAnalysisRunII/panda/macros/80x/NLO/histozllPtRecGen_NLO.root");
-
+void unfolding(int nsel = 0,int LO = 1){
+  TFile *_file0;
+  TFile *_file1;
+  TFile *_file2;
+  if (LO == 0){
+    _file0 = TFile::Open("inputs/histozllPtRec_NLO.root");
+    _file1 = TFile::Open("inputs/histozllPtRecGen_NLO.root");
+  _file2 = TFile::Open("/afs/cern.ch/work/c/ceballos/public/samples/panda/v_004_0/DYJetsToLL_M-50_NLO.root");
   }
   else{
-    TFile *_file0 = TFile::Open("/afs/cern.ch/user/c/christiw/public/panda/MitAnalysisRunII/panda/macros/80x/NLO/histozllPtRec_NLO.root");
-    TFile *_file1 = TFile::Open("/afs/cern.ch/user/c/christiw/public/panda/MitAnalysisRunII/panda/macros/80x/LO/histozllPtRecGen_LO.root");
-
+    _file0 = TFile::Open("inputs/histozllPtRec_LO.root");
+    _file1 = TFile::Open("inputs/histozllPtRecGen_LO.root");
+    _file2 = TFile::Open("/afs/cern.ch/work/c/ceballos/public/samples/panda/v_004_0/DYJetsToLL_M-50_LO.root");
   }
 
 TH1D* bdat = (TH1D*)_file0->Get(Form("histoPtRecDA_%d",nsel));
 TH1D* bini = (TH1D*)_file0->Get(Form("histoPtRecDY_%d",nsel));
 
-
 TH2D* Adet = (TH2D*)_file1->Get(Form("histoPtRecGen_%d",nsel));
-
-TFile *_file2 = TFile::Open("/afs/cern.ch/work/c/ceballos/public/samples/panda/v_004_0/DYJetsToLL_M-50_NLO.root");
 
 TString inputFileHist = "hDDilPtMM";
 if(nsel == 1) inputFileHist = "hDDilPtEE";
@@ -37,26 +37,32 @@ TH1D* xini  = (TH1D*)_file2->Get(inputFileHist.Data());
 TH1D* hDNEvt  = (TH1D*)_file2->Get("hDTotalMCWeight");
 xini->Scale(2008.4*3*35800./hDNEvt->GetSumOfWeights());
 
-printf("%f\n",bdat->GetSumOfWeights());
-printf("%f\n",bini->GetSumOfWeights());
-printf("%f\n",Adet->GetSumOfWeights());
-printf("%f\n",xini->GetSumOfWeights());
+printf("data: %f\n",bdat->GetSumOfWeights());
+printf("  DY: %f\n",bini->GetSumOfWeights());
+printf("  2D: %f\n",Adet->GetSumOfWeights());
+printf(" gen: %f\n",xini->GetSumOfWeights());
 
 int nReg = 5;
 RooUnfoldResponse theResponse(bini, xini, Adet);
 RooUnfoldBayes theRooUnfoldBayes(&theResponse, bdat, nReg);
+theRooUnfoldBayes.SetVerbose(-1);
+theRooUnfoldBayes.SetNToys(1000);
 
-TH1D* hReco= (TH1D*) theRooUnfoldBayes.Hreco();
+TH1D* hReco= (TH1D*) theRooUnfoldBayes.Hreco(RooUnfold::kCovToy);
 theRooUnfoldBayes.PrintTable (cout, xini);
 hReco->Draw();
 hReco->Scale(1,"width");
 //bdat->Draw("SAME");
-xini->SetLineColor(8);
+xini->SetLineColor(2);
 xini->Scale(1,"width");
 xini->Draw("SAME");
 
-//TSVDUnfold *tsvdunf = new TSVDUnfold( bdat, bini, xini, Adet );
-//TH1D* unfresult = tsvdunf->Unfold( 2 );
-//unfresult->Draw();
-//xini->Draw("e,same");
+char output[100];
+sprintf(output,"histoUnfolding_nsel%d_lo%d.root",nsel,LO); 
+TFile* outFilePlots = new TFile(output,"recreate");
+outFilePlots->cd();
+hReco->Write();
+xini->Write();
+outFilePlots->Close();
+
 }
