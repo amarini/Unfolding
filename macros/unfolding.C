63b1749f5e7ac1d15d0004a0f8080f96fac8e256
GuillelmoGomezCeballos <ceballos@cern.ch>
2017-06-22 10:02:26 +0200

    new

--- macros/unfolding.C
+++ macros/unfolding.C
@@ -6,22 +6,22 @@
 #include <TMath.h>
 #include <iostream>
 #include <fstream>
-#include <TSVDUnfold.h>
-#include <TROOT.h>
+#include "TUnfoldDensity.h"
+
 // nsel = 0 (mm) 1 (ee)
 // LO = 0 (NLO) 1 (LO)
-void helper_function(int nsel=0,int LO=1){
+void helper_function(int nsel=0,int LO=1, int rebin=1){
   TFile *_file0;
   TFile *_file1;
   TFile *_file2;
   if (LO == 0){
-    _file0 = TFile::Open("inputs/histozllPtRec_NLO.root");
-    _file1 = TFile::Open("inputs/histozllPtRecGen_NLO.root");
-   _file2 = TFile::Open("/afs/cern.ch/work/c/ceballos/public/samples/panda/v_004_0/DYJetsToLL_M-50_NLO.root");
+    _file0 = TFile::Open("inputs_finerbinning/histozllPtRec_NLO.root");
+    _file1 = TFile::Open("inputs_finerbinning/histozllPtRecGen_NLO.root");
+    _file2 = TFile::Open("/afs/cern.ch/work/c/ceballos/public/samples/panda/v_004_0/DYJetsToLL_M-50_NLO.root");
   }
   else{
-    _file0 = TFile::Open("inputs/histozllPtRec_LO.root");
-    _file1 = TFile::Open("inputs/histozllPtRecGen_LO.root");
+    _file0 = TFile::Open("inputs_finerbinning/histozllPtRec_LO.root");
+    _file1 = TFile::Open("inputs_finerbinning/histozllPtRecGen_LO.root");
     _file2 = TFile::Open("/afs/cern.ch/work/c/ceballos/public/samples/panda/v_004_0/DYJetsToLL_M-50_LO.root");
   }
 
@@ -30,22 +30,29 @@ TH1D* bini = (TH1D*)_file0->Get(Form("histoPtRecDY_%d",nsel));
 
 TH2D* Adet = (TH2D*)_file1->Get(Form("histoPtRecGen_%d",nsel));
 
-TString inputFileHist = "hDDilPtMM";
-if(nsel == 1) inputFileHist = "hDDilPtEE";
+TString inputFileHist = "hDDilLowPtMM";
+if(nsel == 1) inputFileHist = "hDDilLowPtEE";
 TH1D* xini  = (TH1D*)_file2->Get(inputFileHist.Data());
 
 TH1D* hDNEvt  = (TH1D*)_file2->Get("hDTotalMCWeight");
+
 xini->Scale(2008.4*3*35800./hDNEvt->GetSumOfWeights());
+
+bdat->Rebin(rebin);
+bini->Rebin(rebin);
+Adet->Rebin2D(rebin,rebin);
+xini->Rebin(rebin);
 
 printf("data: %f\n",bdat->GetSumOfWeights());
 printf("  DY: %f\n",bini->GetSumOfWeights());
 printf("  2D: %f\n",Adet->GetSumOfWeights());
 printf(" gen: %f\n",xini->GetSumOfWeights());
 
-int nReg = 5;
 RooUnfoldResponse theResponse(bini, xini, Adet);
 theResponse.UseOverflow(kTRUE);
-RooUnfoldBayes theRooUnfoldBayes(&theResponse, bdat, nReg);
+RooUnfoldBayes theRooUnfoldBayes(&theResponse, bdat, 4);
+//RooUnfoldSvd theRooUnfoldBayes(&theResponse, bdat, 20);
+//RooUnfoldTUnfold theRooUnfoldBayes(&theResponse, bdat);
 theRooUnfoldBayes.SetVerbose(-1);
 theRooUnfoldBayes.SetNToys(1000);
 
@@ -68,10 +75,10 @@ outFilePlots->Close();
 
 }
 
-void unfolding(){
+void unfolding(int rebin=10){
   for (int i=0;i<2;i++){
     for (int j=0;j<2;j++){
-      helper_function(i,j);
+      helper_function(i,j,rebin);
 
     }
   }
